34fca9b2b47d9a5ae1f06768ed7f4c2e965d169e
Kobe Dawes <kobe@kobes-mbp.myfiosgateway.com>
2022-03-02 22:22:41 -0500

    code update

--- Triangle.py
+++ Triangle.py
@@ -1,23 +1,20 @@
+"""verifies if the three inputs can form a triangle"""
 
-def classifyTriangle(a,b,c):
-    if not(isinstance(a,int) and isinstance(b,int) and isinstance(c,int)):
+
+def classify_triangle(a, b, c):
+    """verifies if the three inputs can form a triangle"""
+    if not(isinstance(a,int) and isinstance(b,int) and isinstance(c,int)) or a > 200 \
+            or b > 200 or c > 200 or a <= 0 or b <= 0 or c <= 0:
         return 'InvalidInput'
 
-    if a > 200 or b > 200 or c > 200:
-        return 'InvalidInput'
-
-    if a <= 0 or b <= 0 or c <= 0:
-        return 'InvalidInput'
-    
-    if (a + b)<c or (b + c)< a or (c + a)< b:
+    if (a + b)<c or (b + c) < a or (c + a) < b:
         return 'NotATriangle'
-        
-    # now we know that we have a valid triangle 
     if a == b == c == a:
         return 'Equilateral'
-    elif ((a ** 2) + (b ** 2)) == (c ** 2) or ((a ** 2) + (c ** 2)) == (b ** 2) or ((c ** 2) + (b ** 2)) == (a ** 2):
+    if ((a ** 2) + (b ** 2)) == (c ** 2) or ((a ** 2) + (c ** 2)) == (b ** 2) \
+            or ((c ** 2) + (b ** 2)) == (a ** 2):
         return 'Right'
-    elif a != b != c != a:
+    if a != b != c != a:
         return 'Scalene'
     else:
         return 'Isosceles'
